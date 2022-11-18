9ca9fb20bf1fce3eb7c428fd4a0334d530f858e5
IshanJ <72633250+Ishan-Jaidka@users.noreply.github.com>
2022-11-18 14:23:35 -0800

    Add Adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import '../styles/App.css'
 import '../styles/Disclaimer.css'
+import Adsense from '../lib/components/Adsense'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Adsense />
+      <Component {...pageProps} />
+    </>
+  )
 }
