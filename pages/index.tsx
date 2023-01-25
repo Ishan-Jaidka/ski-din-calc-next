39a44ab6315f8818ab63c4085d16c6540c14d823
Ishan Jaidka <ijaidka@gmail.com>
2023-01-24 21:11:05 -0800

    og fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,17 +18,17 @@ function App() {
   return (
     <div className="App">
       <Head>
-        <link rel="icon" href="%PUBLIC_URL%/images/chair2.png" />
+        <link rel="icon" href="https://ski-din-calculator.vercel.app/images/chair2.png" />
         <meta property="og:title" content="Ski DIN Calculator" />
         <meta
           name="description"
           property="og:description"
           content="An educational tool to calculate the DIN (release tension) of your Ski bindings using the 2017 Marker DIN chart."
         />
-        <meta name="image" property="og:image" content="%PUBLIC_URL%/images/chair2.png" />
+        <meta name="image" property="og:image" content="https://ski-din-calculator.vercel.app/images/chair2.png" />
         <meta name="twitter:title" content="Ski DIN Calculator" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="%PUBLIC_URL%/images/chair2.png" />
+        <meta name="twitter:image" content="https://ski-din-calculator.vercel.app/images/chair2.png" />
       </Head>
       <header className="App-header">
         <label>Ski Din Calculator</label>
