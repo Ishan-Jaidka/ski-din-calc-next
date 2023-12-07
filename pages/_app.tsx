74783d4711a2945f1729f2e55d1aede4b51976dc
Ishan Jaidka <ijaidka@gmail.com>
2023-12-06 17:38:39 -0800

    privacy policy update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import '../styles/App.css'
 import '../styles/Disclaimer.css'
+import '../styles/PrivacyPolicy.css'
 import Adsense from '../lib/components/Adsense'
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/privacy_policy.tsx
+++ pages/privacy_policy.tsx
@@ -24,7 +24,56 @@ function App() {
         <label>Privacy Policy</label>
         <Logo />
       </header>
-      <div>We don't collect any user data. We don't keep any information you provide on the website or the app.</div>
+      <div className="Privacy-Policy">
+        Welcome to <b>Ski DIN Calculator</b>. We are committed to respecting the privacy of all visitors/users of our
+        website and app.
+        <br />
+        <br />
+        <br />
+        This Privacy Policy outlines our practices regarding the collection, use, and disclosure of information.
+        <br />
+        <br />
+        <br />
+        <br />
+        <b>Information We Do Not Collect</b>
+        <br />
+        <br />
+        We do not collect any personal data or information from our users. This includes, but is not limited to, names,
+        email addresses, phone numbers, location data, IP addresses, or any other form of personally identifiable
+        information.
+        <br />
+        <br />
+        <br />
+        <br />
+        <b>No Data Storage</b>
+        <br />
+        <br />
+        Our website and app do not store any user data. We operate on a strictly "no data" basis, meaning that all
+        interactions within the website or app are not saved, tracked, or analyzed.
+        <br />
+        <br />
+        <br />
+        <br />
+        <b>Third-Party Links</b>
+        <br />
+        <br />
+        Our website/app may contain links to third-party websites or applications. Please be aware that we are not
+        responsible for the privacy practices of such other sites. We encourage our users to be aware when they leave
+        our site and to read the privacy statements of any other site that collects personally identifiable information.
+        <br />
+        <br />
+        <br />
+        <br />
+        <b>Security Measures</b>
+        <br />
+        <br />
+        Although we do not collect or store any data, we still maintain high standards of digital security to prevent
+        unauthorized access to our website/app infrastructure.
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
       <DownloadApp />
     </div>
   )
